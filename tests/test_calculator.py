bb1b13dd52ae4db65e0cde100656dd6703199656
Raymond Portilla <raymond_porti@hotmail.com>
2026-03-31 09:49:18 -0500

    Add CI file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,6 +3,7 @@ from app.calculator import sum, resta
 def test_sum() -> None:
     assert sum(2, 3) == 5
 
+# Introducir un fallo premeditado!
 def test_resta() -> None:
-    assert resta(5, 3) == 2
+    assert resta(5, 3) == 1
     
